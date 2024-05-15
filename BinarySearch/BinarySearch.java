d37682d909c7ff212f94d55fc89d89ca7027381a
Arnab Naskar <136678992+ArnabNaskar68@users.noreply.github.com>
2024-05-15 17:41:38 +0530

    Add files via upload

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+
+    public static int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) return -1; //Base case.
+
+        // Perform the steps:
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid])
+            return binarySearch(nums, mid + 1, high, target);
+        return binarySearch(nums, low, mid - 1, target);
+    }
+
+    public static int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        int ans = search(a, target);
+        if (ans == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println(ans);
+    }
+}
+
