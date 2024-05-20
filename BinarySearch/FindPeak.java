02254697ee81a4ea740d59f528612faa999aadd4
Arnab Naskar <136678992+ArnabNaskar68@users.noreply.github.com>
2024-05-20 10:32:16 +0530

    Add files via upload

---
+++ BinarySearch/FindPeak.java
@@ -0,0 +1,40 @@
+//Only one peak is available in the array
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FindPeak {
+    public static int findPeakElement(ArrayList<Integer> arr) {
+        int n = arr.size(); // Size of array
+
+        // Edge cases:
+        if (n == 1) return 0;
+        if (arr.get(0) > arr.get(1)) return 0;
+        if (arr.get(n - 1) > arr.get(n - 2)) return n - 1;
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // If arr[mid] is the peak:
+            if (arr.get(mid - 1) < arr.get(mid) && arr.get(mid) > arr.get(mid + 1))
+                return mid;
+
+            // If we are in the left:
+            if (arr.get(mid) > arr.get(mid - 1)) low = mid + 1;
+
+            // If we are in the right:
+            // Or, arr[mid] is a common point:
+            else high = mid - 1;
+        }
+        // Dummy return statement
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr =
+            new ArrayList<>(Arrays.asList(1,2,3,6,9,8,0));
+        int ans = findPeakElement(arr);
+        System.out.println("The peak is at index: " + ans);
+    }
+}
+
