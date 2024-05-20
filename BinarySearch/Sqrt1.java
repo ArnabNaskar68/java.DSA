876045222eec6800481e143920c8f4a006300f0d
Arnab Naskar <136678992+ArnabNaskar68@users.noreply.github.com>
2024-05-20 19:58:15 +0530

    Add files via upload

---
+++ BinarySearch/Sqrt1.java
@@ -0,0 +1,26 @@
+public class Sqrt1 {
+    public static int floorSqrt(int n) {
+        int low = 1, high = n;
+        //Binary search on the answers:
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long val = mid * mid;
+            if (val <= (long)(n)) {
+                //eliminate the left half:
+                low = (int)(mid + 1);
+            } else {
+                //eliminate the right half:
+                high = (int)(mid - 1);
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int n = 28;
+        int ans = floorSqrt(n);
+        System.out.println("The floor of square root of " + n + " is: " + ans);
+    }
+}
+
+
